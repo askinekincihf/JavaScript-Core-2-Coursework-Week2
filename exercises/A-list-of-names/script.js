1a7e33241711ec3354cafcf7a60dee009bde6a74
askinekinci <askinekincihf@gmail.com>
2021-02-21 19:55:26 +0000

    completed A-list-of-names and B-shopping-list.

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,6 +1,22 @@
+// Solution - 1 
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach(person => {
+    content.innerHTML += `<h1>${person.name}</h1><h2>${person.job}</h2>`;
+  })
 }
+
+// Solution - 2
+// function listOfNames(arrayOfPeople) {
+//   let content = document.querySelector("#content");
+//   arrayOfPeople.forEach(person => {
+//     const h1Tags = document.createElement("h1");
+//     const h2Tags = document.createElement("h2");
+//     content.append(h1Tags, h2Tags);
+//     h1Tags.innerText = person.name;
+//     h2Tags.innerText = person.job;
+//   })
+// }
 
 let people = [
   { name: "Chris", job: "Teacher" },
--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,6 +1,22 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+// Solution - 1
+function shoppingList(arrayOfShopping) {
+  const content = document.querySelector("div");
+  const unOrderedList = document.createElement("ul");
+  content.appendChild(unOrderedList);
+  arrayOfShopping.forEach(item => unOrderedList.innerHTML += `<li>${item}</li>` );
 }
+
+// Solution - 2
+// function shoppingList(arrayOfShopping) {
+//   const content = document.querySelector("div");
+//   const unOrderedList = document.createElement("ul");
+//   content.appendChild(unOrderedList);
+//   arrayOfShopping.forEach((element) => {
+//     let list = document.createElement("li");
+//     list.innerText = element;
+//     unOrderedList.appendChild(list);
+//   })
+// }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
