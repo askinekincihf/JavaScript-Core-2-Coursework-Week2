1efa601634bb35f31288d9d7fd7f26379a3ed009
askinekinci <askinekincihf@gmail.com>
2021-02-22 21:49:34 +0000

    E-list-of-colours

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,15 @@
 function listOfColours(colours) {
-  // Write your code here...
+  const content = document.querySelector("#content");
+  const selectElement = document.createElement("select");
+  const para = document.createElement("p");
+  content.append(selectElement, para);
+  colours.forEach(color => {
+    selectElement.innerHTML += `<option>${color}</option>`;
+  });
+  selectElement.addEventListener("change", (e) => {
+    para.textContent = `You have selected: ${e.target.value}`
+    para.style.backgroundColor = e.target.value;
+  });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
